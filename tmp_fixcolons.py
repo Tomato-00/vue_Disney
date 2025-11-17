3d28e1f22ab3b091d5c8c1a504cc5d1f28551acd
tomato <861014649@qq.com>
2025-11-17 21:17:17 +0800

    删除py

--- tmp_fixcolons.py
+++
@@ -1,7 +0,0 @@
-﻿from pathlib import Path
-path = Path('src/views/Home/IndexPage.vue')
-text = path.read_text(encoding='utf-8')
-print(text.count(':::v-deep'))
-print(text.count('::v-deep'))
-text = text.replace(':::v-deep', '::v-deep')
-path.write_text(text, encoding='utf-8')
--- tmp_inspect.py
+++
@@ -1,7 +0,0 @@
-﻿from pathlib import Path
-path = Path('src/views/Home/IndexPage.vue')
-for i,line in enumerate(path.read_text(encoding='utf-8').splitlines()):
-    if 'deep' in line:
-        print(i+1, repr(line))
-        if i > 330:
-            break
